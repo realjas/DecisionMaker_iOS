7b53d717af97a5089e306acc30464a1873b4a9f6
realjas <realjas@hotmial.com>
2019-04-02 17:07:49 -0400

    Fist commit

---
+++ DecisionMaker/ViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ViewController.swift
+//  DecisionMaker
+//
+//  Created by realJAS on 6/19/16.
+//  Copyright © 2016 decisionmakerproject. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var option1: UITextField!
+    @IBOutlet var option2: UITextField!
+    @IBOutlet var option3: UITextField!
+    @IBOutlet var option4: UITextField!
+    @IBOutlet var option5: UITextField!
+    
+    //@IBOutlet var answer: UITextField!
+    
+    @IBOutlet var popupAnswerImage: UIImageView!
+    @IBOutlet var popupAnswer: UILabel!
+    
+    @IBOutlet var decideButton: UIButton!
+    
+    @IBOutlet var hideKeyboardHintText: UILabel!
+    
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    @IBOutlet var overlay: UILabel!
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        self.overlay.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.spinner.alpha = 0.0
+        self.spinner.stopAnimating()
+        self.popupAnswer.alpha = 0.0
+        self.popupAnswerImage.alpha = 0.0
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            self.hideKeyboardHintText.alpha = 0.0
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func decide(_ sender: UIButton) {
+        self.overlay.backgroundColor = UIColor(white: 1, alpha: 0.6)
+        self.spinner.alpha = 1.0
+        self.spinner.startAnimating()
+        
+        play("hmm")
+        
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(ViewController.setTimeout(_:)), userInfo: nil, repeats: false)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+        
+        self.popupAnswer.alpha = 0.0
+        self.popupAnswerImage.alpha = 0.0
+        self.overlay.backgroundColor = UIColor(white: 1, alpha: 0.0)
+    }
+    
+    func setTimeout(_ timer : Timer) {
+        self.spinner.alpha = 0.0
+        self.spinner.stopAnimating()
+        
+        self.popupAnswer.alpha = 1.0
+        self.popupAnswerImage.alpha = 1.0
+        
+        play("cheer")
+        
+        var optionsArray = [String]()
+        optionsArray = [self.option1.text!,self.option2.text!,self.option3.text!,self.option4.text!,self.option5.text!]
+        var blankChecker = [String]()
+        var pos = 0
+        var txt = ""
+        
+        for i in 0...optionsArray.count-1 {
+            if optionsArray[i] != "" {
+                txt = optionsArray[i]
+                blankChecker.append("")
+                blankChecker[pos] = txt
+                pos += 1
+            }
+        }
+        if pos != 0 {
+            let r = Int(arc4random_uniform(UInt32(blankChecker.count)))
+            //self.answer.text = blankChecker[r]
+            self.popupAnswer.text = blankChecker[r].uppercased() + "!"
+        }else{
+            blankChecker.append("Yes.")
+            blankChecker.append("No.")
+            let r = Int(arc4random_uniform(UInt32(blankChecker.count)))
+            
+            self.popupAnswer.text = blankChecker[r]
+            //self.popupAnswer.text = "Seriously?!?!"
+        }
+    }
+    
+    func play(_ sound: String){
+        let audioPathString = Bundle.main.path(forResource: sound, ofType: "mp3")
+        if let audioPathString = audioPathString {
+            let audioPathURL = URL(fileURLWithPath: audioPathString)
+            do{
+                try audioPlayer = AVAudioPlayer(contentsOf: audioPathURL)
+                audioPlayer.volume = 0.65
+                audioPlayer.play()
+            }catch{
+                print("Sound Error")
+            }
+        }
+    }
+}
+
